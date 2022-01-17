ad399b71a6350f6eab8d750a6fde807f7f03381b
Johnpope7 <Johnpope5387@gmail.com>
2022-01-16 19:54:44 -0700

    Updated PlayerPawn Regions
    updated regions to include the Generql pawn Attributes region

--- Assets/Script/Pawns/PlayerPawn.cs
+++ Assets/Script/Pawns/PlayerPawn.cs
@@ -11,7 +11,6 @@ public class PlayerPawn : Pawn
     protected float comboMeterMax; //the maximum amount of points needed to fill the combo meter
     [SerializeField, Tooltip("The amount of extra lives the player has")]
     protected float lives; //stores the amount of extra lives the player has
-
  
     protected bool shapeShifted; //the boolean for deciding of the player is shape shifted into wolf form or not
     protected bool isSprinting; //the boolean determining if the player is sprinting or not
