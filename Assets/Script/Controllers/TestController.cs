3fa14263a7cc8a6293d7309f4bf57566d63a788c
Johnpope7 <Johnpope5387@gmail.com>
2022-01-21 14:54:42 -0700

    Move input Halfway setup
    Move function has been halfway setup, pushing so that other programmers can take a look at the issue

--- Assets/Script/Controllers/TestController.cs
+++ Assets/Script/Controllers/TestController.cs
@@ -17,6 +17,8 @@ public class TestController : MonoBehaviour
     [Header("General Variables")]
     protected Rigidbody2D rb2d; //the rigidbody of the test character
     protected PlayerPawn pawn; //the PlayerPawn class so that we can access the player characters movement data
+    protected PlayerInput playerInput; //defines the input that the pawn is utlizing 
+    protected PlayerInputActions playerInputActions; //variable for storing the input schemes the pawn will be using
     [SerializeField]
     protected LayerMask groundLayer; //The layer mask for what is considered "the ground" in the game
     #endregion
@@ -28,30 +30,58 @@ public class TestController : MonoBehaviour
     protected Transform groundCheck; //the specified location that decides whether the pawn is touching the ground or not
     [SerializeField]
     protected float circleRadius; //determines the size of the capsule at the feet of the pawn
+    [Header("Lateral Movement Variables")]
+    protected int walkVelocity; //the stand in variable for walk speed on this script
+    protected int runVelocity; //the stand in variable for run speedon this script
     #endregion
     #endregion
 
     #region Functions
-    protected virtual void Start()
+    protected virtual void Awake()
     {
         pawn = GetComponent<PlayerPawn>(); //defines the pawn needed for all test character stats
         rb2d = GetComponent<Rigidbody2D>(); //defines the Gameobjects needed for the test character
+        playerInput = GetComponent<PlayerInput>(); //defines the initial input system being used by the pawn
+
+        #region Player Action Subscriptions
+        PlayerInputActions playerInputActions = new PlayerInputActions(); //initializes te players inputs
+        playerInputActions.PlayerHuman.Enable(); //enables the players input from the specfied input actions
+        playerInputActions.PlayerHuman.Jump.performed += Jump; //subscribes to the jump function
+        #endregion
     }
-    protected virtual void Update() 
+    protected virtual void FixedUpdate() 
     {
+        Vector2 inputVector = playerInputActions.PlayerHuman.Move.ReadValue<Vector2>();
         grounded = Physics2D.OverlapCircle(groundCheck.position, circleRadius, groundLayer); //this update checks to see if the player is grounded
     }
-    public virtual void Jump()
+    public virtual void Jump(InputAction.CallbackContext context)
     {
-        if (grounded) //only allows the player to jump if they're on the ground
+        Debug.Log(context);
+        if (context.performed)
         {
-            verticalVelocity = pawn.JumpHeight; //sets the verticalVelocity variable equal to that of the protected variable jumpHeight on playerpawn
-            Debug.Log("Vertical Velocity is: " + verticalVelocity); 
-            Debug.Log("Jump Height is: " + pawn.JumpHeight);
-            rb2d.velocity = Vector2.up * verticalVelocity; //applies velocity to the upward vector causing the character to jump
-            Debug.Log("Jumped!");
+            if (grounded) //only allows the player to jump if they're on the ground
+            {
+                Debug.Log("Jump! " + context.phase);
+                verticalVelocity = pawn.JumpHeight; //sets the verticalVelocity variable equal to that of the protected variable jumpHeight on playerpawn
+                rb2d.velocity = Vector2.up * verticalVelocity; //applies velocity to the upward vector causing the character to jump
+            }
         }
     }
+
+   /*
+    public virtual void Move(InputAction.CallbackContext context) 
+    {
+        if (grounded) 
+        {
+            Debug.Log(context);
+            walkVelocity = pawn.WalkSpeed; //sets the walkVelocity variable equal to that of the protected variable _walkSpeed on the playerpawn
+            Debug.Log("Walk Velocity is: " + walkVelocity);
+            Debug.Log("Walk Speed is: " + pawn.WalkSpeed);
+            Vector2 inputVector = context.ReadValue<Vector2>(); //acquires the movement direction we are applying to the rigidbody through the controller input
+            rb2d.velocity = new Vector2(inputVector.x, inputVector.y) * walkVelocity; //moves the character back and forth and multiplies the movement by the walkVelocity
+        }
+    }
+   */
 
     #endregion
 
