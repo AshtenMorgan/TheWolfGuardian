6fc1b3310b8578d14c983234ef062bdcc754375a
Christopher Cadwell <chrcadwe@uat.edu>
2022-01-24 19:54:35 -0700

    Created CameraController
    Switched control of main camera to script, as opposed to being a child of player object

---
+++ Assets/Script/Controllers/CameraController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    #region variables
+    Vector3 cameraPos = Vector3.zero;           //Variable for camera location
+    public Transform player;                    //The players position
+    public float dampTime = 0.2f;               //how fast the camera tracks the player
+    private Vector3 velocity = Vector3.zero;    //movement speed
+#endregion
+
+    // Update is called once per frame
+    void Update()
+    {
+        cameraPos = new Vector3(player.position.x, player.position.y, -10f);//store player position as vector3
+        transform.position = Vector3.SmoothDamp(gameObject.transform.position, cameraPos, ref velocity, dampTime);//move camera to match player, delayed a bit by dampTime
+
+    }
+}
