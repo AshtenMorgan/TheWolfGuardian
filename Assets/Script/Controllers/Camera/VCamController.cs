06963390ff92699baa35b2e4679fac8dd79674e9
Christopher Cadwell <69337965+ChristopherCadwell@users.noreply.github.com>
2022-04-26 21:43:34 -0700

    Update for vcam controller
    updating controller to allow for transitions into confiners

--- Assets/Script/Controllers/Camera/VCamController.cs
+++ Assets/Script/Controllers/Camera/VCamController.cs
@@ -2,16 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
+using Cinemachine.Utility;
 
 public class VCamController : MonoBehaviour
 {
     private GameManager gm;
     private CinemachineVirtualCamera vcam;
     public float pixelsPerUnit;
+    public CinemachineConfiner confiner;
 
     // Start is called before the first frame update
     void Start()
     {
+   
         gm = GameManager.Instance; //assign gm var
         vcam = GetComponent<CinemachineVirtualCamera>(); //find virtual cam component
         vcam.LookAt = gm.player.transform; // set lookat target
@@ -28,5 +31,12 @@ public class VCamController : MonoBehaviour
     {
         Vector3 vectorInPixels = new Vector3(Mathf.CeilToInt(moveVector.x * pixelsPerUnit), Mathf.CeilToInt(moveVector.y * pixelsPerUnit), Mathf.CeilToInt(moveVector.z * pixelsPerUnit));
         return vectorInPixels / pixelsPerUnit;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<SuperGrid>())
+        {
+            confiner.m_BoundingShape2D = collision.GetComponent<PolygonCollider2D>();
+        }
     }
 }
