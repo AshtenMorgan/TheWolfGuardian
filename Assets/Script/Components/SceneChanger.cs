a9004d7082067b21cf7c9ddeb73027262a1b5653
Hyrum-J <97466306+Hyrum-J@users.noreply.github.com>
2022-11-13 12:58:43 -0700

    Added the scene changer script
    We can now change scenes by adding this to whatever door object we want. Can be modified for buttons and such if we need to later.

---
+++ Assets/Script/Components/SceneChanger.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public string Destination;
+
+    private GameObject target;
+
+    private PlayerControllerV2 controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        target = collision.gameObject;
+        controller = target.GetComponent<PlayerControllerV2>();
+        Debug.Log("Made it to getting component");
+        if (controller != null)
+        {
+            Debug.Log("Should Change");
+            SceneManager.LoadScene(Destination);
+        }
+    }
+}
