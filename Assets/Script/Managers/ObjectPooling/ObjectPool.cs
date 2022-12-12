2c1cb311c74f3c83c44844ea13c7745d330857ba
CCadwell3 <58010842+CCadwell3@users.noreply.github.com>
2022-12-12 01:46:38 -0700

    Changed pool setup
    Object pool is now created from the spawnpoints added into the scene, everything is automatic, and the spawnpoints are set up with sprites

--- Assets/Script/Managers/ObjectPooling/ObjectPool.cs
+++ Assets/Script/Managers/ObjectPooling/ObjectPool.cs
@@ -1,101 +1,96 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
-[RequireComponent(typeof(RoomManager))]
 public class ObjectPool : MonoBehaviour
 {
     #region variables
-    public enum Enemies
-    {
-        Redcap,
-        Shadow_Widow,
-        Spider_Test
-    }
-    
     #region Pools
-    [HideInInspector]
-    public List<GameObject> enemyPool;
-    private string[] _prefabLocation;
-    [Header("All enemies you wish to spawn"), Tooltip("Select 1 type per enemy spawn")]
-    public Enemies[] m_enemies;
-    #endregion
+    [HideInInspector] public List<GameObject> enemyPool;
 
+    [SerializeField, HideInInspector] private List<string> _prefabLocation;
+    [SerializeField, HideInInspector] private List<GameObject> _allPoints;
+    #endregion
     #region Game Objects
-    private Transform instanPoint;
-    [HideInInspector]
-    public GameObject[] enemy;
-    [Header("Spawn Points - Match this to the number of enemies you set Up there ^^")]
-    public Transform[] spawnPoint;
+    [Header("Instantiation point - 1 per room")]
+    public Transform instanPoint;
 
+    //hide these after testing
+    public List<GameObject> enemy;
+    [Header("This should show all of the spawn points")]
+    public List<Transform> spawnPoints;
     #endregion
-
-
     #endregion
 
     #region Functions
     private void Start()
     {
-        enemy = new GameObject[m_enemies.Length];
-        instanPoint = this.transform;
-        EnumToString();
+        GetSpawnPoints();
+        CreateSpawnLists();
+        PoolSetup();
     }
     #region Enemy Pool checks
     public void PoolSetup()
     {
         //create pool lists
         enemyPool = new List<GameObject>();
-        
+
         #region Object Creation
         //initial spawning for object pooling
         //enemies
-        // Enemy1 = Resources.Load("Prefabs/Test Prefabs/Test Enemies/Enemy1") as GameObject;
-        for (int i = 0; i < enemy.Length; i++)
+        for (int i = 0; i < _allPoints.Count; i++)
         {
-            enemy[i] = Resources.Load(_prefabLocation[i]) as GameObject;
+            enemy.Add(Resources.Load(_prefabLocation[i]) as GameObject);
         }
 
         GameObject tmp;//create a temporary game object to hold each new instantiated object
 
         //stick enemies into pool
-        for (int i = 0; i < enemy.Length; i++)
+        for (int i = 0; i < enemy.Count; i++)
         {
             tmp = Instantiate(enemy[i], instanPoint.position, instanPoint.rotation) as GameObject;//instantiate enemy, hold it in tmp GO
             tmp.SetActive(false);//hide and disable the enemy
             enemyPool.Add(tmp);//add enemy to pool
         }
-            
-        
-       
+
         #endregion
     }
-    void EnumToString()
-    {
-        _prefabLocation = new string[m_enemies.Length];
-        for (int i = 0; i < m_enemies.Length; i++)
-        {
-            switch (m_enemies[i])
-            {
-                case Enemies.Redcap:
-                    _prefabLocation[i] = "Prefabs/Pawn Prefabs/Beastiary Prefabs/Unseelie Fey/Beast_UF_Redcap";
-                    break;
-
-                case Enemies.Shadow_Widow:
-                    _prefabLocation[i] = "Prefabs/Pawn Prefabs/Beastiary Prefabs/Spiders/Beast_SP_Shadow";
-                    break;
-
-                case Enemies.Spider_Test:
-                    _prefabLocation[i] = "Prefabs/Test Prefabs/Test Enemies/SpiderNewTest";
-                
-                    break;
-
-                default:
-                    break;
-            }
-        }
-        PoolSetup();
-    }
-  
     #endregion
+    public void GetSpawnPoints()
+    {
+        Transform[] t = gameObject.GetComponentsInChildren<Transform>();//create an array of child transforms
+
+
+        foreach (Transform child in t)
+        {
+            if (child.CompareTag("EnemySpawn"))
+                _allPoints.Add(child.gameObject);
+        }
+
+        spawnPoints = new List<Transform>();
+        for (int i = 0; i < _allPoints.Count; i++)
+        {
+            spawnPoints.Add(_allPoints[i].GetComponent<Transform>());
+        }
+    }
+    void CreateSpawnLists()
+    {   //set up enemy types
+        _prefabLocation = new List<string>();
+
+        for (int i = 0; i < _allPoints.Count; i++)
+        {
+            Debug.Log("Loop " + i + " of " + _allPoints.Count);
+            if (_allPoints[i].GetComponent<Beast_UF_Redcap>())//redcap
+                _prefabLocation.Add("Prefabs/Pawn Prefabs/Beastiary Prefabs/Unseelie Fey/Beast_UF_Redcap");
+            
+            else if (_allPoints[i].GetComponent<Beast_SP_Webspitter>())//spider
+                _prefabLocation.Add("Prefabs/Pawn Prefabs/Beastiary Prefabs/Spiders/Beast_SP_Webspitter");
+            else
+                return;
+        }
+
+    }
+
     #endregion
 }
